729eb3986ad539a04edcea47194962d9c20da03c
Mikuts <mikuts2007@gmail.com>
2025-06-07 18:41:44 +0300

    Pievienots sākuma logs

--- src/Sakums.java
+++ src/Sakums.java
@@ -3,6 +3,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.ImageIcon;
 
 public class Sakums extends JFrame {
 
@@ -29,12 +34,31 @@ public class Sakums extends JFrame {
 	 * Create the frame.
 	 */
 	public Sakums() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 690, 436);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Sākt");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		
+		btnNewButton.setBounds(235, 336, 196, 50);
+		 btnNewButton.setBackground(new Color(229, 229, 229));
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Teksta Datnes Tests");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 42));
+		lblNewLabel.setBounds(141, 240, 386, 41);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("java1.png"));
+		lblNewLabel_1.setBounds(118, 0, 426, 234);
+		contentPane.add(lblNewLabel_1);
 	}
-
 }
